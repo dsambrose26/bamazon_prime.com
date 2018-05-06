dae11ae6bbdea8098e1efd434fab0a3e148e97a3
Derek Ambrose <derek_ambrose@me.com>
2018-05-06 18:05:09 -0400

    first prompt working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,76 +1,68 @@
-//Install this module everytime to check for .env file to protect secrets and it makes sure secrets are added to my environment and not my source code
-//Never commit .env file to github. Create .gitignore that includes the .env file so it is not added to github
-//require("dot-env");
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var table = require("console.table");
-
+// mySQL connection
 var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'root',
-	database: 'bamazon_db'
+	host: "localhost",
+	port: "3306",
+	user: "root",
+	password: "",
+	database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-	if(err) throw err;
-	display();
-});
-
-//Function is displaying items for sale: ids, names, and prices
-function display() {
-	connection.query("SELECT * FROM products", function(err, res) {
-		if (err) throw err;
-		console.table(res);
-
-//Order
-	inquirer
-		.prompt([
-			{
-	 		name: "choice",
-	       	type: "input",
-	    	message: "Which Product ID?"
-       		},
-       		{
-			name: "number",
-	       	type: "input",
-	        message: "How many of this Product?"
-       		}
-			])
-		
-//Answer for quantity in stock of chosen item
-       	.then(function(answer) {
-       		var chosenItem;
-           		for (var i=0; i < res.length; i++) {
-					   if(res[i].item_id === answer.choice) { 
-						   chosenItem = res[i];
+connection.query("SELECT * FROM products", function (err, res) {
+					if (err) throw err;
+					console.log("Item    Product \t\tDepartment \tPrice\t Stock");
+					console.log("------------------------------------------------------------------");
+						for (var i = 0; i < res.length; i++) {
+						console.log(res[i].ItemID + " \t" + res[i].ProductName + "\t" + res[i].DepartmentName + "\t" + res[i].Price + " \t " + res[i].StockQuantity); 
 						}
-       				}
-       			if(chosenItem.stock_quantity < parseInt(answer.number)){
-       			console.log("Item(s) is/are available")
+						console.log("------------------------------------------------------------------");
+					
+				inquirer.prompt([{
+					name: "product",
+					type: "input",
+					message: "What is the ID of the product that works best for you? [Changed your mind? Press Y]"
+					}, 
+				{
+				
+			}]).then(function (ansProd) {
+				if (ansProd.product.toUpperCase() == "Y") { connection.end(); return; }
+					else {
+						inquirer.prompt([{
+							name: "qty",
+							type: "input",
+							message: "You will claim your write-off later, so how many of this product did you have in mind?"
 
-//Update database
-connection.query("UPDATE products SET ? WHERE ?",[
-    {
-       	stock_quantity: number
-    },
-    {
-       	item_id: chosenItem
-    }],
-       	function(error) {
-       		if (error) throw err;
-       		console.log("Benefits purchased! Check your email and follow the instructions on completing this purchase.");
-       		display();
-       	}
-    );	
-	}
+			}]).then(function (ansQty) {
+				console.log(ansProd.product);
+					
+connection.query('SELECT * FROM products WHERE ?', [{ ProductName: ansProd.product }], function (err, res) {
+					if (err) throw err;
+					if (res[0].StockQuantity > ansQty.qty) {
+		
+				var cost = res[0].Price * ansQty.qty
+				console.log("-----------------------------------");
+				console.log("Your order is validated! \nThe total cost is $" + cost.toFixed(2) + "\nThank you for ordering")
+		
+				var newQty = res[0].StockQuantity - ansQty.qty
+
+connection.query("UPDATE products SET ? WHERE ?", [{
+					StockQuantity: newQty
+					}, 
+				{
+					ProductName: ansProd.product
+					}], 
 	
-//Log if quanitity is not available        		
-       		else {
-       			console.log("Insufficient Quantity!");
-       			display();
-       	}		
-    });
-})};
+					function (err, res) { 
+					});
+				} 
+				else {
+					console.log("-----------------------------------");
+					console.log("Sorry, we do not have enough in stock. \nWe only have " + res[0].StockQuantity + " units of " + ansProd.product + ". \nPlease retry your order. \nThank you!")
+					}			
+				})
+			})
+		}
+	})
+});
---
+++ bamazon_db.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+	-- Columns
+	ItemID INT NOT NULL AUTO_INCREMENT,
+	ProductName VARCHAR(200) NULL,
+	DepartmentName VARCHAR(200) NULL,
+	Price INT(5) DEFAULT 0,
+	StockQuantity INT (5) NOT NULL,
+  
+  PRIMARY KEY (ItemID)
+);
+
+select * from products;
+
+-- 10 sample products
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Miosoft SOAR DVD", "Miosoft Ed", 12, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Starter Architect", "DSA® Suite", 5000, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Extended SOAR Pack", "DSA® Suite", 1000, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Customization Pack", "Config & Dev", 6000, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Extended HR/Payroll", "HR Management", 7000, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Runtime SOAR Server", "Databases", 30000, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Standard Runtime CAL", "Databases", 50000, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Full User License", "Miosoft Ed", 3000, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Limited User License", "Miosoft Ed", 300, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Self Serve User CAL", "Miosoft Ed", 60, 5);
+
+SELECT * FROM products;
